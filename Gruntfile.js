e62183c419da78d9e45739f15e02d021b1f874f5
Paul Gray <pgray@learningobjects.com>
2016-02-23 10:50:18 -0500

    Adding Dockerfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -185,12 +185,11 @@ module.exports = function (grunt) {
 
   grunt.registerTask('serve', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'open:dist', 'connect:dist']);
+      return grunt.task.run(['build', 'connect:dist']);
     }
 
     grunt.task.run([
       'clean:dist',
-      'open:dev',
       'copy:dev',
       'sass:dev',
       'concurrent:dev'
